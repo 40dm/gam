e027a777c5328d4f833b27482565cce7059fa7ed
Jon-Marshall Vaile <36483384+jmarshvaile@users.noreply.github.com>
2023-07-15 22:04:32 -0500

    a little cleanup
    removed unused variable, defined variables with let, added semi-colons

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ Array.from(buttons).forEach(e => e.addEventListener('click', move))
 
 // I personally hate this function but I have yet to learn a better way to get the ID of a clicked element.
 // If you know a better way, just do it and I will learn.
-function move(elementId) {
-    elementId = window.event;
-    target = elementId.target;
-    let selection = target.id
-    console.log(`User clicked: ${selection}.`)
+function move() {
+    let elementId = window.event;
+    let target = elementId.target;
+    let selection = target.id;
+    console.log(`User clicked: ${selection}.`);
 }
