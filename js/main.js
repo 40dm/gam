f01e9553878ceb88400ee8055e45931b145e3750
jmarshvaile <jmarshvaile@gmail.com>
2023-07-16 10:51:38 -0500

    revert modules change

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-import { Canvas } from "js/canvas.js";
+class Canvas {
+    constructor (id) {
+        this.element = document.getElementById(id);
+        this.element.width = window.innerWidth;
+        this.element.height = window.innerHeight;
+        this.draw = this.element.getContext("2d");
+    }
+}
 
 // Create a new canvas
 let canvas = new Canvas("test");
