ce6e0b009eef21b7a54be7fe1841bf42081e17d2
jmarshvaile <jmarshvaile@gmail.com>
2023-07-16 21:52:34 -0500

    continue to improve modules for drawing objects

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 import { Canvas } from "./modules/canvas.js";
+import { Circle } from "./modules/circle.js";
 
 // Creates new canvas
-let canvas = new Canvas("test", document.body);
+let canvas = new Canvas("test");
 canvas.create();
 canvas.width = 600;
 canvas.height = 300;
-
 canvas.context.fillStyle = "black";
 canvas.context.fillRect(0, 0, canvas.width, canvas.height);
 
-// TODO: create a listener to update canvas size on window resizing
-
-// Just a test shape
-canvas.context.beginPath();
-canvas.context.arc(canvas.width / 2, 
-                canvas.height / 2, 
-                Math.min(canvas.width, canvas.height) / 30, 
-                0, 2 * Math.PI);
-canvas.context.strokeStyle = "white";
-canvas.context.stroke();
+// Creates new circle
+let circle = new Circle(canvas.context, canvas.center.x, canvas.center.y, 5, "white");
+circle.draw();
 
 // TODO: create movement of the circle using WASD
