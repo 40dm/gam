b35e7fde11015fa07488e923eec409429263e5f9
Jon-Marshall Vaile <36483384+jmarshvaile@users.noreply.github.com>
2023-07-18 16:48:30 -0500

    added relative position improvements and integration with movement fn (#37)
    * added relative position improvements and integration with movement function
    
    * remove some logging

--- main.js
+++ main.js
@@ -3,64 +3,74 @@ import { Circle } from './modules/circle.js';
 import { Rectangle } from './modules/rectangle.js';
 
 
-// Creates resizable canvas with relative shape sizes
+// Creates canvas and shapes
 let canvas = new Canvas('game', document.body);
-let rectangle = new Rectangle(canvas);
-let circle = new Circle(canvas);
+let scene = {
+    model: new Rectangle(canvas),
+    pos: { x: 0, y: 0 },
+    size: 1.0,
+    color: 'darkblue',
+}
+let player = {
+    model: new Circle(canvas),
+    pos: { x: 0, y: 0 },
+    size: 0.05,
+};
+let enemy = {
+    model: new Rectangle(canvas),
+    pos: { x: -0.25, y: 0.25 },
+    size: 0.05,
+    color: 'black',
+};
 
-let draw = new ResizeObserver(elements => {
-    elements.forEach(element => {
-        canvas.draw();
-        rectangle.draw();
-        circle.draw();
-    });
-});
-draw.observe(canvas.parent);
+// Draws and redraws canvas and shapes
+new ResizeObserver(draw).observe(canvas.parent);
+function draw() {
+    canvas.draw();
+    scene.model.draw(scene.pos.x, scene.pos.y, scene.size, scene.size, scene.color, true);
+    enemy.model.draw(enemy.pos.x, enemy.pos.y, enemy.size, enemy.size, enemy.color, false);
+    player.model.draw(player.pos.x, player.pos.y, player.size);
+}
 
-// This creates the event listeners on the window listening for keypresses/releases. 
-window.addEventListener('keydown', keysPressed, false);
+// This creates the event listeners on the window listening for keypresses/releases.
+window.addEventListener('keydown', event => { keysPressed(event, player.pos); draw() }, false);
 window.addEventListener('keyup', keysReleased, false);
 
-let deltaX = 0;
-let deltaY = 0;
 // Simply storing the keycode values in a more readable format.
 const keyW = 87;
 const keyA = 65;
 const keyS = 83;
 const keyD = 68;
-let keys = [];
+let keys = {};
 
 // Removed the switch statement for handling keypresses and instead I'm using an if statement now
-// TODO: pass deltaX and deltaY into some kind of redrawing method/function for moving the object.
-function keysPressed(key) {
+function keysPressed(key, position) {
     // store an entry for every key pressed
     keys[key.keyCode] = true;
+    
     // left movement
     if (keys[keyA]) {
-        deltaX -= 2;
-        console.log(`User pressed ${key}. Object moved: ${deltaX}`)
+        position.x -= 0.01;
+        console.log(`User pressed ${key}. Object moved: ${position.x}`)
     }
     // right movement
     if (keys[keyD]) {
-        deltaX += 2;
-        console.log(`User pressed ${key}. Object moved: ${deltaX}`)
+        position.x += 0.01;
+        console.log(`User pressed ${key}. Object moved: ${position.x}`)
     }
     //down movement 
     if (keys[keyS]) {
-        deltaY -= 2;
-        console.log(`User pressed ${key}. Object moved: ${deltaY}`)
+        position.y -= 0.01;
+        console.log(`User pressed ${key}. Object moved: ${position.y}`)
     }
     // up movement 
     if (keys[keyW]) {
-        deltaY += 2;
-        console.log(`User pressed ${key}. Object moved: ${deltaY}`)
+        position.y += 0.01;
+        console.log(`User pressed ${key}. Object moved: ${position.y}`)
     }
 
     // prevents default system behavior from being triggered by keypresses in the window
     key.preventDefault();
-
-    // redraw function goes here in the future
-    // e.g. drawSomething();
 }
 
 function keysReleased(key) {
