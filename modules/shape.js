7c0f34f5a4237e2cc0d6c0f54b7b18c764de7515
jmarshvaile <jmarshvaile@gmail.com>
2023-07-20 20:14:10 -0500

    get static, dynamic, and stretch values working

--- modules/shape.js
+++ modules/shape.js
@@ -1,19 +1,90 @@
-class Shape {
-    #width;
-    #height;
+export class Shape {
+    // Creates persistent memory
+    #m = Object.fromEntries(
+        ['parent', 'static', 'dynamic', 'stretch'].map(p => [p, {}])
+    );
+
+    // Defines class properties at object initialization
     constructor(canvas) {
-        this.canvas = canvas;
-        this.context = canvas.getContext('2d');
-        this.fixed = {};
-        Object.defineProperties(this.fixed, {
-            width: {
-                set: (width) => (this.#width = width),
-                get: () => this.context,
+        
+        // Handles access of parent attributes
+        this.parent = {};
+        Object.defineProperties(this.parent, {
+            w: {
+                get: () => document.body.clientWidth,
+                enumerable: true,
             },
-            height: {
-                set: (height) => (this.#height = height),
-                get: () => this.context,
-            }
+            h: {
+                get: () => document.body.clientHeight,
+                enumerable: true,
+            },
+        });
+
+        // Handles access of static attributes
+        this.static = {};
+        Object.defineProperties(this.static, {
+            w: {
+                set: (w) => {
+                    Object.keys(this.#m).forEach(k => this.#m[k].w = w);
+                    this.#m.parent.w = this.parent.w;
+                },
+                get: () => Math.round(this.#m.static.w * this.#m.parent.w),
+                enumerable: true,
+            },
+            h: {
+                set: (h) => {
+                    Object.keys(this.#m).forEach(k => this.#m[k].h = h);
+                    this.#m.parent.h = this.parent.h;
+                },
+                get: () => Math.round(this.#m.static.h * this.#m.parent.h),
+                enumerable: true,
+            },
+        });
+
+        // Handles access of dynamic attributes
+        this.dynamic = {};
+        Object.defineProperties(this.dynamic, {
+            w: {
+                set: (w) => {
+                    Object.keys(this.#m).forEach(k => this.#m[k].w = w);
+                    this.#m.parent.w = this.parent.w;
+                },
+                get: () =>  Math.round(
+                    this.#m.dynamic.w * Math.min(this.parent.w, this.parent.h)
+                ),
+                enumerable: true,
+            },
+            h: {
+                set: (h) => {
+                    Object.keys(this.#m).forEach(k => this.#m[k].h = h);
+                    this.#m.parent.w = this.parent.w;
+                },
+                get: () =>  Math.round(
+                    this.#m.dynamic.h * Math.min(this.parent.w, this.parent.h)
+                ),
+                enumerable: true,
+            },
+        });
+
+        // Handles access of stretch attributes
+        this.stretch = {};
+        Object.defineProperties(this.stretch, {
+            w: {
+                set: (w) => {
+                    Object.keys(this.#m).forEach(k => this.#m[k].w = w);
+                    this.#m.parent.w = this.parent.w;
+                },
+                get: () => Math.round(this.#m.stretch.w * this.parent.w),
+                enumerable: true,
+            },
+            h: {
+                set: (h) => {
+                    Object.keys(this.#m).forEach(k => this.#m[k].h = h);
+                    this.#m.parent.h = this.parent.h;
+                },
+                get: () => Math.round(this.#m.stretch.h * this.parent.h),
+                enumerable: true,
+            },
         });
     }
 }
