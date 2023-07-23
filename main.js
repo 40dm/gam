d0682800dc5b7d7924120d3844f9a57ba90068e9
jmarshvaile <jmarshvaile@gmail.com>
2023-07-23 01:10:28 -0500

    adds touch screen input handling

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ import { Canvas } from './modules/canvas.js';
 import { Rectangle } from './modules/rectangle.js';
 
 // Creates the scene
-let canvas = new Canvas( 'canvas', document.body )
-canvas.draw();
+let scene = new Canvas( 'scene', document.body )
+scene.draw();
 
 // Creates scene background
-let background = new Rectangle( canvas, 'stretch' );
+let background = new Rectangle( scene, 'stretch' );
 background.width = 1;
 background.height = 1;
 background.color = 'darkslategray';
 background.draw();
 
 // Creates hostile npc
-let hostile = new Rectangle( canvas, 'dynamic' );
+let hostile = new Rectangle( scene, 'dynamic' );
 hostile.width = .05;
 hostile.height = .05;
 hostile.x = -.3;
@@ -22,7 +22,7 @@ hostile.color = 'orangered';
 hostile.draw();
 
 // Creates player character
-let player = new Rectangle( canvas, 'dynamic' );
+let player = new Rectangle( scene, 'dynamic' );
 player.width = .05;
 player.height = .05;
 player.color = 'midnightblue';
@@ -39,17 +39,49 @@ let speed = 15;
 let delay = 1000 / fps;
 let frame = () => {
     // Draws scene
-    canvas.draw();
+    scene.draw();
 
     // Draws shapes
     background.draw();
     player.draw();
     hostile.draw();
 
-    // Sets player movement
+    // Sets player movement (WASD)
     if ( isKeyDown[ 'KeyW' ] ) player.y -= .01 * ( speed / fps );
     if ( isKeyDown[ 'KeyS' ] ) player.y += .01 * ( speed / fps );
     if ( isKeyDown[ 'KeyA' ] ) player.x -= .01 * ( speed / fps );
     if ( isKeyDown[ 'KeyD' ] ) player.x += .01 * ( speed / fps );
+
+    // Sets player movement (touch)
+    if ( isTouched.up ) player.y -= .01 * ( speed / fps );
+    if ( isTouched.down ) player.y += .01 * ( speed / fps );
+    if ( isTouched.left ) player.x -= .01 * ( speed / fps );
+    if ( isTouched.right ) player.x += .01 * ( speed / fps );
 }
 setInterval( frame, delay );
+
+// Handles touch start and end events
+let isTouched = { start: {} };
+let fingerSize = 60;
+scene.element.addEventListener( 'touchstart', ( e ) => { 
+    let x = Math.round( e.changedTouches[0].pageX / fingerSize ) * fingerSize;
+    let y = Math.round( e.changedTouches[0].pageY / fingerSize ) * fingerSize;
+    isTouched.start.x = Math.round( x / fingerSize ) * fingerSize;
+    isTouched.start.y = Math.round( y / fingerSize ) * fingerSize;
+});
+scene.element.addEventListener( 'touchmove', ( e ) => { 
+    let x = Math.round( e.changedTouches[0].pageX / fingerSize ) * fingerSize;
+    let y = Math.round( e.changedTouches[0].pageY / fingerSize ) * fingerSize;
+    isTouched.up = y < isTouched.start.y;
+    isTouched.down = y > isTouched.start.y;
+    isTouched.left = x < isTouched.start.x;
+    isTouched.right = x > isTouched.start.x;
+});
+scene.element.addEventListener( 'touchend', ( e ) => { 
+    isTouched.start.x = undefined;
+    isTouched.start.y = undefined;
+    isTouched.up = false;
+    isTouched.down = false;
+    isTouched.left = false;
+    isTouched.right = false;
+});
